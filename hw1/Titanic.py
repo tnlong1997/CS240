d1ed0f11642a01b6921898a48adcda358d39e4d6
tnlong1997 <tnlong1997@gmail.com>
2018-02-01 15:12:36 -0500

    HW1

---
+++ hw1/Titanic.py
@@ -0,0 +1,48 @@
+import csv
+
+class Passenger(object):
+    def __init__(self, id, surname, pclass, survived, sex):
+        self.id = id
+        self.surname = surname
+        self.pclass = pclass
+        self.survived = survived
+        self.sex = sex
+
+    def __repr__(self):
+        return "(%d) %s, %s, %s, %s" % (
+            self.id,
+            self.surname,
+            ["1st", "2nd", "3rd"][self.pclass - 1],
+            ["Died", "Survived"][self.survived],
+            ["Male", "Female"][self.sex]
+        )
+
+    def is_firstclass(self):
+        return self.pclass == 1
+
+    def survived(self):
+        return self.survived
+
+
+def make_passengers():
+    idi = 0
+    namei = 1
+    pclassi = 2
+    survivedi = 5
+    sexcodei = 6
+    passengers = []
+    with open('./Titanic.csv', 'r') as csvfile:
+        titanic_reader = csv.reader(csvfile)
+        for row in titanic_reader:
+            id = int(row[idi])
+            surname = row[namei].split(',')[0]
+            pclass = {'1st' : 1, '2nd' : 2, '3rd' : 3}[row[pclassi]]
+            survived = int(row[survivedi])
+            sex = int(row[sexcodei])
+            passengers.append(Passenger(id, surname, pclass, survived, sex))
+    return passengers
+
+
+class Titanic(object):
+    def __init__(self):
+        self.passengers = make_passengers()
---
+++ hw1/starter.py
@@ -0,0 +1,30 @@
+import Titanic
+
+def omega():
+    # Write your code to generate omega here
+    pass
+
+passengers = omega()
+
+def survived(pasengers):
+    # Write your code to generate the set of survivors here
+    pass
+
+def first_class(passengers):
+    # Write your code to generate the set of survivors here
+    pass
+
+def second_class(passengers):
+    pass
+
+def third_class(passengers):
+    pass
+
+def p_first_class_survivors(passengers):
+    # Write your code to compute the probability that a first class passenger survives here
+    pass
+
+def p_survivor_given_first_class(passengers):
+    # Write your code to compute the probability that a passenger survives, given that they are
+    # trvelling first class here
+    pass
