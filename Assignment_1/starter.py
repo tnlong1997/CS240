ee9eb6ea5d66213d29ea33782d503d27e08e0c24
tnlong1997 <tnlong1997@gmail.com>
2018-02-09 17:42:18 -0500

    Assignment 1

--- Assignment_1/starter.py
+++ Assignment_1/starter.py
@@ -1,30 +1,73 @@
 import Titanic
 
+
 def omega():
     # Write your code to generate omega here
-    pass
+    omega = Titanic.Titanic()
+    print(len(omega.passengers))
+    return omega.passengers
+
 
 passengers = omega()
 
+
 def survived(pasengers):
     # Write your code to generate the set of survivors here
-    pass
+    s = []
+    for passenger in passengers:
+        if passenger.survived:
+            s = s + [passenger]
+    print(len(s))
+    return s
+
+
+s = survived(passengers)
+
 
 def first_class(passengers):
     # Write your code to generate the set of survivors here
-    pass
+    f = []
+    for passenger in passengers:
+        if passenger.is_firstclass():
+            f = f + [passenger]
+    print(len(f))
+    return f
+
+
+f = first_class(passengers)
+
 
 def second_class(passengers):
     pass
 
+
 def third_class(passengers):
     pass
 
+
 def p_first_class_survivors(passengers):
-    # Write your code to compute the probability that a first class passenger survives here
-    pass
+    # Write your code to compute the probability
+    # that a first class passenger survives here
+    count = 0
+    for passenger in passengers:
+        if passenger.is_firstclass() and passenger.survived:
+            count += 1
+    print(count / len(passengers))
+    return count / len(passengers)
+
+
+p_first_class_survivors(passengers)
+
 
 def p_survivor_given_first_class(passengers):
-    # Write your code to compute the probability that a passenger survives, given that they are
-    # trvelling first class here
-    pass
+    # Write your code to compute the probability that a passenger survives,
+    # given that they are travelling first class here
+    count = 0
+    for passenger in passengers:
+        if passenger.survived:
+            count += 1
+    print(count / len(passengers))
+    return count / len(passengers)
+
+
+p_survivor_given_first_class(f)
